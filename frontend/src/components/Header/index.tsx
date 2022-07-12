47b1ab2078d2bf65f27d456fc423aeb23cbde449
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-07-11 21:16:40 -0300

    DatePicker

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import image from "../../assets/img/header.svg"
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={image} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+            </p>
+        </div>
+    </header>
+    )
+}
+
+export default Header
